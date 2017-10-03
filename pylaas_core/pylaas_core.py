224c6f2b5d250f1a9f3b59f920c1d3b4a2f47fbb
fschneider <fs@agidev.fr>
2017-10-03 10:39:15 +0200

    add some docstring

--- pylaas_core/pylaas_core.py
+++ pylaas_core/pylaas_core.py
@@ -1,7 +1,7 @@
 """
 This module implements the alfred core system.
 
-Pylaas(<config: file|{}>)
+PylaasCore.init(<definitions: file|{}>)
 """
 from pylaas_core.technical.container import Container
 
@@ -18,16 +18,37 @@ class PylaasCore:
         """
         raise TypeError("PylaasCore class may not be instantiated")
 
+    @classmethod
     def init(cls, definitions):
+        """Init AlfredCore
+
+        init default Container with definitions
+
+        Args:
+            definitions (dict): container definitions
+        """
         cls._container = Container()
         cls._container.add_definition(definitions)
 
+    @classmethod
     def get_container(cls):
+        """Get current container
+
+        Returns:
+            container (ContainerInterface): current container
+
+        """
         return cls._container
 
+    @classmethod
     def get_service(cls, service_id):
-        return cls.get_container().get(service_id)
+        """Get service
 
-    init = classmethod(init)
-    get_container = classmethod(get_container)
-    get_service = classmethod(get_service)
+        Args:
+            service_id (string): service id to instantiate from container
+
+        Returns:
+            service (ServiceInterface): service instance (singletons)
+
+        """
+        return cls.get_container().get(service_id)
